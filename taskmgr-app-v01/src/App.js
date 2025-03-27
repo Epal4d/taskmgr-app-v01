2415fa6a7d68c5e678bd39dfb0b0df01a813bd5e
evan.al4d <evan.al4d@gmail.com>
2025-03-27 14:03:02 -0500

    Add Home

--- taskmgr-app-v01/src/App.js
+++ taskmgr-app-v01/src/App.js
@@ -1,7 +1,9 @@
+import Home from "./components/Home";
+
 function App() {
   return (
     <>
-    <p> Demo Task Manager app </p>
+      <Home/>
     </>
   );
 }
---
+++ taskmgr-app-v01/src/components/Home.js
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <>
+            <p>Demo Task Manager App</p>
+            <p>Home Page </p>
+        </>
+    );
+}
+
+export default Home; 
