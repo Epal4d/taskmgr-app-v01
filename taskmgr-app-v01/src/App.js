a1643f1da53a47ff2d3f087c0ddf5827a59c3130
evan.al4d <evan.al4d@gmail.com>
2025-03-27 16:58:07 -0500

    update

--- taskmgr-app-v01/src/App.js
+++ taskmgr-app-v01/src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <>
       <HashRouter>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/contact" element={<Contact/>}/>
