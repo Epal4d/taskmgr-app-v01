2e6afb9f8e408528027b08c80850b05897da0f78
evan.al4d <evan.al4d@gmail.com>
2025-03-27 17:15:52 -0500

    update Contact pg

--- taskmgr-app-v01/src/components/Contact.js
+++ taskmgr-app-v01/src/components/Contact.js
@@ -2,7 +2,16 @@ const Contact = () => {
     return(
     <>
 
-     <h1>Contact Me</h1>;
+     <h1>Contact Me</h1>
+     <form>
+        <div>
+            <span>First Name:</span> <input type="text" maxlength={25} placeholder="First Name"/> 
+            <span>Last Name:</span> <input type="text" maxlength={25} placeholder="Last Name"/> 
+            <span>Email:</span> <input type="text" maxlength={25} placeholder="Email"/> 
+            <span>Comment:</span> <input type="text" maxlength={100} placeholder="Comment"/> 
+            <button>Submit</button> <a href="#">Clear</a>
+        </div>
+     </form>
 
     </>
     )
