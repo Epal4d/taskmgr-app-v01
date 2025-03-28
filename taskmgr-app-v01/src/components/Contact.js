ce3458d493a6e6d73967da70e4407cc613769693
evan.al4d <evan.al4d@gmail.com>
2025-03-28 13:31:03 -0500

    updateContact

--- taskmgr-app-v01/src/components/Contact.js
+++ taskmgr-app-v01/src/components/Contact.js
@@ -1,14 +1,20 @@
 const Contact = () => {
+
+    useEffect(()=>{ 
+        console.log("#App::Contact page load")
+
+
+    },[])
     return(
     <>
 
      <h1>Contact Me</h1>
      <form>
         <div>
-            <span>First Name:</span> <input type="text" maxlength={25} placeholder="First Name"/><br></br> 
-            <span>Last Name:</span> <input type="text" maxlength={25} placeholder="Last Name"/> <br></br>
-            <span>Email:</span> <input type="text" maxlength={25} placeholder="Email"/> <br></br>
-            <span>Comment:</span> <input type="text" maxlength={100} placeholder="Comment"/> <br></br>
+            <span>First Name:</span> <input type="text" maxLength={25} placeholder="First Name"/><br></br> 
+            <span>Last Name:</span> <input type="text" maxLength={25} placeholder="Last Name"/> <br></br>
+            <span>Email:</span> <input type="text" maxLength={25} placeholder="Email"/> <br></br>
+            <span>Comment:</span> <input type="text" maxLength={100} placeholder="Comment"/> <br></br>
             <button>Submit</button> <a href="#">Clear</a>
         </div>
      </form>
--- taskmgr-app-v01/src/components/Home.js
+++ taskmgr-app-v01/src/components/Home.js
@@ -3,11 +3,24 @@ import Footer from "./Footer"
 import { useEffect } from "react" 
 function Home()
 {
+    const [tmplist, setTodoList]
+
+
     useEffect(()=>{ 
         console.log("#App::Home page load")
 
+        // setTodoList(tmplist)
+        // console.log("---todolist/n",todolist)
+
 
     },[])
+
+    const initial_todo_items = [
+        {id:1,name:"item 1", completed:false}
+        {id:2,name:"item 2", completed:false}
+        {id:3,name:"item 3", completed:false}
+        {id:4,name:"item 4", completed:false}
+    ]
 
 
     return(
@@ -16,6 +29,10 @@ function Home()
         
             <p>Demo Task Manager App</p>
             <p>Home Page </p>
+
+
+            
+            
 
             <Footer title="footer" year={2025}></Footer>
         </>
